329f64a01b70a99759715a3a26f3761b5fd5c4aa
Li Yao <yaoli.email@gmail.com>
2020-10-03 02:16:39 -0700

    better

--- tracking/UKF/src/main.cpp
+++ tracking/UKF/src/main.cpp
@@ -14,11 +14,13 @@ int main() {
    */
   // The state vector is 5D of [P_x, P_y, v, phi, phi_dot].
   // Therefore, the number of sigma points is 2*5+1=11
-  MatrixXd Xsig = MatrixXd(5, 11);
-  ukf.GenerateSigmaPoints(&Xsig);
+  //MatrixXd Xsig = MatrixXd(5, 11);
+  //ukf.GenerateSigmaPoints(&Xsig);
+  MatrixXd Xsig_aug = MatrixXd(7, 15);
+  ukf.AugmentedSigmaPoints(&Xsig_aug);
        
   // print result
-  std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+  std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
 
   return 0;
 }
