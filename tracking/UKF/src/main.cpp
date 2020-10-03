35aed7d4986a6bd30f1b30392c88755c66b666f8
Li Yao <yaoli.email@gmail.com>
2020-10-03 12:18:44 -0700

    better

--- tracking/UKF/src/main.cpp
+++ tracking/UKF/src/main.cpp
@@ -16,11 +16,15 @@ int main() {
   // Therefore, the number of sigma points is 2*5+1=11
   //MatrixXd Xsig = MatrixXd(5, 11);
   //ukf.GenerateSigmaPoints(&Xsig);
-  MatrixXd Xsig_aug = MatrixXd(7, 15);
-  ukf.AugmentedSigmaPoints(&Xsig_aug);
+  
+  //MatrixXd Xsig_aug = MatrixXd(7, 15);
+  //ukf.AugmentedSigmaPoints(&Xsig_aug);
+
+  MatrixXd Xsig_pred = MatrixXd(15, 5);
+  ukf.SigmaPointPrediction(&Xsig_pred);
        
   // print result
-  std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
+  //std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
 
   return 0;
 }
