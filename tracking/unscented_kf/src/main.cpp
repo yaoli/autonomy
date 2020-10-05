172d504cd8c35a8a9262091b72685ac42d28b198
Li Yao <yaoli.email@gmail.com>
2020-10-04 23:26:14 -0700

    better

--- tracking/unscented_kf/src/main.cpp
+++ tracking/unscented_kf/src/main.cpp
@@ -27,9 +27,14 @@ int main() {
   //MatrixXd P_pred = MatrixXd(5, 5);
   //ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
 
-  VectorXd z_out = VectorXd(3);
-  MatrixXd S_out = MatrixXd(3, 3);
-  ukf.PredictRadarMeasurement(&z_out, &S_out);
+  //VectorXd z_out = VectorXd(3);
+  //MatrixXd S_out = MatrixXd(3, 3);
+  //ukf.PredictRadarMeasurement(&z_out, &S_out);
+
+  VectorXd x_out = VectorXd(5);
+  MatrixXd P_out = MatrixXd(5, 5);
+  ukf.UpdateState(&x_out, &P_out);
+  
   // print result
   //std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
 
